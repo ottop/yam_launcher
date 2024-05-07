71338fb9f1621f946f8f76f9d533e55b3adbbd75
ottoptj <ottop@ottop.eu>
2024-05-07 07:38:34 +0300

    Added a long-press menu (info, delete, hide, rename). Rename not done. Others work.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("com.google.code.gson:gson:2.10")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/eu/ottop/yamlauncher/AppMenuActivity.kt
+++ app/src/main/java/eu/ottop/yamlauncher/AppMenuActivity.kt
@@ -1,34 +1,29 @@
 package eu.ottop.yamlauncher
 
 import android.content.Context
+import android.content.Intent
+import android.content.pm.ApplicationInfo
 import android.content.pm.LauncherActivityInfo
 import android.content.pm.LauncherApps
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.os.UserHandle
-import android.util.Log
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.appcompat.app.AppCompatActivity
 import eu.ottop.yamlauncher.databinding.ActivityAppMenuBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.util.SortedMap
 
 class AppMenuActivity : AppCompatActivity() {
 
@@ -40,10 +35,11 @@ class AppMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityAppMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(null)
-        shownApps = listOf<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>>()
+        shownApps = listOf()
         searchView = findViewById(R.id.searchView)
         container = findViewById(R.id.container)
 
@@ -62,51 +58,12 @@ class AppMenuActivity : AppCompatActivity() {
 
         })
 
-        binding.root.addOnLayoutChangeListener { view, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+        binding.root.addOnLayoutChangeListener { _, _, top, _, bottom, _, oldTop, _, oldBottom ->
             if (bottom - top > oldBottom - oldTop) {
                 searchView.clearFocus()
             }
         }
 
-    }
-
-    override fun onStop() {
-        super.onStop()
-        checkApps?.cancel()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        startTask()
-    }
-
-    private fun startTask() {
-        checkApps = GlobalScope.launch {
-            while (true) {
-                if (!listsEqual(shownApps, getInstalledPersonalApps())) {
-                    Log.d("AHAHHAHAA","AHJAHAHA")
-                    shownApps = getInstalledPersonalApps()
-                    runOnUiThread {
-                        deleteAppMenuContents()
-                        createPersonalAppMenu()
-                    }
-
-                }
-                delay(1000)
-            }
-        }
-    }
-
-    private fun listsEqual(list1: List<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>>, list2: List<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>>): Boolean {
-        if (list1.size != list2.size) return false
-
-        for (i in list1.indices) {
-            if (list1[i].first.componentName != list2[i].first.componentName || list1[i].second.first != list2[i].second.first) {
-                return false
-            }
-        }
-
-        return true
     }
 
     private fun filterItems(query: String?) {
@@ -128,30 +85,101 @@ class AppMenuActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        checkApps?.cancel()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startTask()
+    }
+
+    private fun startTask() {
+        checkApps = GlobalScope.launch {
+            while (true) {
+                if (!listsEqual(shownApps, getInstalledApps())) {
+                    shownApps = getInstalledApps()
+                    runOnUiThread {
+                        refreshAppMenu()
+                    }
+
+                }
+                delay(1000)
+            }
+        }
+    }
+
+    private fun listsEqual(
+        list1: List<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>>,
+        list2: List<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>>
+    ): Boolean {
+        if (list1.size != list2.size) return false
+
+        for (i in list1.indices) {
+            if (list1[i].first.componentName != list2[i].first.componentName || list1[i].second.first != list2[i].second.first) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    private fun refreshAppMenu() {
+        deleteAppMenuContents()
+        createAppMenu()
+    }
+
+    private fun deleteAppMenuContents(): Boolean {
+        binding.container.removeAllViewsInLayout()
+        return true
+    }
+
+    private fun createAppMenu(): Boolean {
+        val apps = getInstalledApps()
+        apps.forEach { appInfo ->
+            createAppText(appInfo.first, appInfo.second.first, appInfo.second.second)
+        }
+        return true
+    }
+
     private fun getInstalledApps(): List<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>> {
         val allApps = mutableListOf<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>>()
         val launcherApps = this.getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
         for (i in launcherApps.profiles.indices) {
             launcherApps.getActivityList(null, launcherApps.profiles[i]).forEach { app ->
-                allApps.add(Pair(app, Pair(launcherApps.profiles[i], i)))
+                if (!isAppHidden(app.activityInfo.applicationInfo.packageName, i)) {
+                    allApps.add(Pair(app, Pair(launcherApps.profiles[i], i)))
+                }
             }
-
         }
-        return allApps.sortedBy { it.first.label.toString().lowercase() }
+        return allApps.sortedBy {
+            it.first.applicationInfo.loadLabel(packageManager).toString().lowercase()
+        }
     }
 
-    private fun getInstalledPersonalApps(): List<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>> {
-        return getInstalledApps()
-    }
-
-    private fun getInstalledWorkApps(): List<Pair<LauncherActivityInfo, Pair<UserHandle, Int>>> {
-        return getInstalledApps()
-    }
-
-    private fun createAppMenu(appInfo: LauncherActivityInfo, userHandle: UserHandle, workProfile: Int): Boolean {
+    private fun createAppText(
+        appInfo: LauncherActivityInfo,
+        userHandle: UserHandle,
+        workProfile: Int
+    ): Boolean {
         val appInfo = appInfo.activityInfo.applicationInfo
-        val textView = TextView(this)
 
+        val textView = TextView(this)
+        val launcherApps = getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
+        val mainActivity =
+            launcherApps.getActivityList(appInfo.packageName, userHandle).firstOrNull()
+
+        setupTextView(textView, appInfo, workProfile)
+
+        setupTextListeners(textView, appInfo, userHandle, workProfile, launcherApps, mainActivity)
+
+        binding.container.addView(textView)
+
+        return true
+    }
+
+    private fun setupTextView(textView: TextView, appInfo: ApplicationInfo, workProfile: Int) {
         val states = arrayOf(
             intArrayOf(-android.R.attr.state_pressed),
             intArrayOf(android.R.attr.state_pressed)
@@ -164,57 +192,136 @@ class AppMenuActivity : AppCompatActivity() {
 
         with(textView) {
             textSize = 28f
-            setPadding(0,0,0,50)
+            setPadding(0, 10, 0, 80)
             isClickable = true
             focusable = View.FOCUSABLE
             gravity = Gravity.START
 
-            if (workProfile != 0) {
-                text = "*"+appInfo.loadLabel(packageManager)
-            }
-            else {
-                text = appInfo.loadLabel(packageManager)
+            text = if (workProfile != 0) {
+                "*" + appInfo.loadLabel(packageManager)
+            } else {
+                appInfo.loadLabel(packageManager)
             }
             setTextColor(ColorStateList(states, colors))
         }
+    }
 
-
+    private fun setupTextListeners(
+        textView: TextView,
+        appInfo: ApplicationInfo,
+        userHandle: UserHandle,
+        workProfile: Int,
+        launcherApps: LauncherApps,
+        mainActivity: LauncherActivityInfo?
+    ) {
         textView.setOnLongClickListener {
-            Toast.makeText(this, "Long press detected on ${appInfo.loadLabel(packageManager)}", Toast.LENGTH_SHORT).show()
-
-            true
+            appActionMenu(textView, appInfo, userHandle, workProfile, launcherApps, mainActivity)
         }
 
         textView.setOnClickListener {
-            val launcherApps = getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
-            val packageName = appInfo.packageName
-
-            val mainActivity = launcherApps.getActivityList(packageName, userHandle).firstOrNull()
-
             if (mainActivity != null) {
                 launcherApps.startMainActivity(mainActivity.componentName, userHandle, null, null)
             } else {
-                Toast.makeText(this, "Unable to launch ${appInfo.loadLabel(packageManager)}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Unable to launch ${appInfo.loadLabel(packageManager)}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun appActionMenu(
+        textView: TextView,
+        appInfo: ApplicationInfo,
+        userHandle: UserHandle,
+        workProfile: Int,
+        launcherApps: LauncherApps,
+        mainActivity: LauncherActivityInfo?
+    ): Boolean {
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = inflater.inflate(R.layout.app_action_menu, null)
+
+        val popupWindow = PopupWindow(
+            popupView,
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        popupWindow.isOutsideTouchable = true
+        popupWindow.isFocusable = true
+
+        popupWindow.animationStyle = android.R.style.Animation_Translucent
+
+        if (appInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0) {
+            popupView.findViewById<TextView>(R.id.uninstall).visibility = View.GONE
+        }
+
+        textView.visibility = View.INVISIBLE
+
+        popupWindow.showAsDropDown(textView, 0, -textView.height)
+
+        popupWindow.setOnDismissListener {
+            textView.visibility = View.VISIBLE
+        }
+
+        popupView.findViewById<TextView>(R.id.info).setOnClickListener {
+            if (mainActivity != null) {
+                launcherApps.startAppDetailsActivity(
+                    mainActivity.componentName,
+                    userHandle,
+                    null,
+                    null
+                )
             }
 
+            popupWindow.dismiss()
         }
 
-        // Add the button to the LinearLayout
-        binding.container.addView(textView)
+        popupView.findViewById<TextView>(R.id.uninstall).setOnClickListener {
+            val intent = Intent(Intent.ACTION_DELETE)
+            intent.data = Uri.parse("package:${appInfo.packageName}")
+            intent.putExtra(Intent.EXTRA_USER, userHandle)
+            startActivity(intent)
 
-        return true
-    }
-
-    private fun createPersonalAppMenu(): Boolean {
-        val pApps = getInstalledPersonalApps()
-        pApps.forEach { appInfo ->
-            createAppMenu(appInfo.first, appInfo.second.first, appInfo.second.second)
+            popupWindow.dismiss()
         }
-        return true
+
+        popupView.findViewById<TextView>(R.id.rename).setOnClickListener {
+            // Handle rename action
+            popupWindow.dismiss()
+        }
+
+        popupView.findViewById<TextView>(R.id.hide).setOnClickListener {
+            setAppHidden(appInfo.packageName, workProfile, true)
+            refreshAppMenu()
+
+            popupWindow.dismiss()
+        }
+
+        return true // Indicate that the long click event is consumed}
     }
 
-    private fun deleteAppMenuContents(): Boolean {
-        binding.container.removeAllViewsInLayout()
-        return true
+    private fun setAppHidden(packageName: String, profile: Int, hidden: Boolean) {
+        // Get the shared preferences editor
+        val editor = getSharedPreferences("app_data", MODE_PRIVATE).edit()
+        val key = "$packageName-${profile}"
+        editor.putBoolean(key, hidden)
+        editor.apply()
+    }
+
+    private fun isAppHidden(packageName: String, profile: Int): Boolean {
+        // Get the shared preferences object
+        val sharedPref = getSharedPreferences("app_data", MODE_PRIVATE)
+        val key = "$packageName-${profile}"
+        return sharedPref.getBoolean(key, false) // Default to false (visible)
+    }
+
+    private fun setAppVisible(packageName: String, profile: Int) {
+        // Get the shared preferences editor
+        val editor = getSharedPreferences("app_data", MODE_PRIVATE).edit()
+        val key = "$packageName-${profile}"
+        editor.remove(key)
+        editor.apply()
     }
 }
