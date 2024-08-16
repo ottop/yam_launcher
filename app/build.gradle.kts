548c74327f2edeb6aa300b83dd6b2faf12df03f2
ottoptj <ottop@ottop.eu>
2024-08-16 16:47:05 +0300

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "eu.ottop.yamlauncher"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.6-test"
+        versionCode = 3
+        versionName = "0.7"
     }
 
     dependenciesInfo {
