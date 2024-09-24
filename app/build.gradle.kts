c4a5f34cf301f2e3e448210885a57e542e393259
ottoptj <ottop@ottop.eu>
2024-09-25 00:13:07 +0300

    Updated the version and added a changelog.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "eu.ottop.yamlauncher"
         minSdk = 31
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.4"
+        versionCode = 9
+        versionName = "1.5"
     }
 
     dependenciesInfo {
