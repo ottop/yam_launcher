51df64514955b832cda104e5cf57f0c0dd3058da
CreativeCodeCat <wayne6324@gmail.com>
2025-04-21 17:58:27 +0100

    Upgrade Gradle to 8.11.1 and update dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     defaultConfig {
         applicationId = "eu.ottop.yamlauncher"
         minSdk = 31
+        //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 12
         versionName = "1.7"
@@ -49,11 +50,12 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.activity:activity-ktx:1.9.3")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.recyclerview)
+    implementation(libs.preference.ktx)
+    implementation(libs.activity.ktx)
+    implementation(libs.constraintlayout)
+    implementation(libs.biometric.ktx)
 }
